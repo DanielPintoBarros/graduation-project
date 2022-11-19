a2f432c983458a3ab8565e565d0c95173daa3419
DanielPintoBarros <danielpbarros.95@gmail.com>
2022-11-19 17:04:59 -0300

    fix chart

--- web-interface/src/components/Report/LineChart.js
+++ web-interface/src/components/Report/LineChart.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 function LineChart({ chartData }) {
   return <Line data={chartData} />;
